cffeda76a15b624b4e3bab6b0544c10a0563f65d
max3d <vadim@unikivi.ee>
2019-06-04 23:13:06 +0300

    redux provider, store and mock reducer added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import React from "react";
 import {View, Text} from "react-native";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from "./reducers"
 
-export default class extends React.Component {
+export default class App extends React.Component {
 
     render() {
-        return <View><Text>This is a text message</Text></View>
+        return <Provider store={createStore(reducers)}>
+            <View>
+                <Text>This is a text message</Text>
+            </View>
+        </Provider>
 
     }
 }
