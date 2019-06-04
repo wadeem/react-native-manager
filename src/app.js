d6fb007f06c30f9639acb6a879011a25bdb5fb13
max3d <vadim@unikivi.ee>
2019-06-04 22:56:32 +0300

    redux added, app.js refactored

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default class extends React.Component {
+
+    render() {
+        return <View><Text>This is a text message</Text></View>
+
+    }
+}
