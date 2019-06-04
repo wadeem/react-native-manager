e10e9ecb954b120040a884002bf6570191ea8e79
max3d <vadim@unikivi.ee>
2019-06-04 23:57:41 +0300

    firebase init in app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import {View, Text} from "react-native";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import reducers from "./reducers"
+import firebaseConfig from "./config/firebase.js";
+import firebase from "firebase";
 
 export default class App extends React.Component {
+
+    componentWillMount() {
+        firebase.initializeApp(firebaseConfig);
+    }
 
     render() {
         return <Provider store={createStore(reducers)}>
